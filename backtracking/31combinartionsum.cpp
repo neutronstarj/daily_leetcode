7408ad7c536c3ab570f75671b3d05e538f33625f
Iris He <113053257+neutronstarj@users.noreply.github.com>
2024-07-20 10:46:21 -0600

    Create 31combinartionsum.cpp

---
+++ backtracking/31combinartionsum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(vector<int>& candidates, int target, int sum, int startIndex) {
+      //the two terminate condition is when sum=target or sum>target
+        if (sum > target) {
+            return;
+        }
+        if (sum == target) {
+            result.push_back(path);
+            return;
+        }
+
+        for (int i = startIndex; i < candidates.size(); i++) {
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            backtracking(candidates, target, sum, i); // 不用i+1了，表示可以重复读取当前的数
+          //Do not use i+1 because we can have repeated number 
+            sum -= candidates[i];
+            path.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        result.clear();
+        path.clear();
+        backtracking(candidates, target, 0, 0);
+        return result;
+    }
+};
